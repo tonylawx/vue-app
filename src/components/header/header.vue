dbc402a2914c9024b214a59ae4dd57c953d76e1f
Tony Law <tonylawx@tom.com>
2017-01-11 01:00:50 +0800

    头部组件可见样式完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -21,8 +21,13 @@
           <i class="icon-keyboard_arrow_right"></i>
         </div>
       </div>
+    </div>
     <div class="bulletin-wrapper">
-
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class= "icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
     </div>
   </div>
 </template>
@@ -42,8 +47,9 @@ export default {
 <style lang="scss" rel="stylesheet/scss" >
   @import "../../common/sass/mixin";
 .header {
+  position: relative;
   color:#fff ;
-  background-color: #999;
+  background: rgba(7,17,27,0.5);
   .content-wrapper{
     position: relative;
     padding:24px 12px 18px 24px;
@@ -139,6 +145,47 @@ export default {
         font-size: 10px;
       }
     }
+  }
+  .bulletin-wrapper{
+    position: relative;
+    height: 28px;
+    line-height: 28px;
+    padding:0 22px 0 12px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 0;
+    background: rgba(7,17,27,0.2);
+    .bulletin-title{
+      display: inline-block;
+      vertical-align: top;
+      margin-top: 7px;
+      width: 22px;
+      height: 12px;
+      @include bg-img('bulletin');
+      background-size:22px 12px;
+      background-repeat: no-repeat;
+    }
+    .bulletin-text{
+      vertical-align: top;
+      font-size: 10px;
+      margin: 0 4px;
+    }
+    .icon-keyboard_arrow_right{
+      position: absolute;
+      font-size: 10px;
+      right: 12px;
+      top: 8px;
+    }
+  }
+  .background{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index:-1;
+    filter:blur(10px)
   }
 }
 
