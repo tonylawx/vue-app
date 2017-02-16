d7943ecbfd2fe9755873264671ac6ef15ed07aec
SingularityXD <tonylawx@tom.com>
2017-02-16 22:42:15 +0800

    goods组件拖动部分完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
-    <div class="menu-warpper">
+    <div class="menu-warpper" ref=menuWarpper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item,$index) in goods" class="menu-item" :class="{'current':currentIndex===$index}">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-warpper">
+    <div class="foods-warpper" ref=foodsWarpper>
       <ul>
-        <li v-for='item in goods' class="food-list">
+        <li v-for='item in goods' class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -22,12 +22,11 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span>
-                  <span v-show='food.oldPrice' class="old">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span v-show='food.oldPrice'
+                                                                class="old">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -38,6 +37,7 @@
   </div>
 </template>
 <script type='text/ecmascript-6'>
+  import BScroll from "better-scroll";
   const ERR_OK = 0;
   export default {
     props: {
@@ -47,7 +47,9 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
     },
     created() {
@@ -56,8 +58,47 @@
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
+          this.$nextTick(() => {
+            this._initScroll();
+            this._calculateHeight();
+          });
         }
       });
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$refs.menuWarpper, {});
+        this.foodsScroll = new BScroll(this.$refs.foodsWarpper, {
+          probeType: 3
+        });
+
+        this.foodsScroll.on("scroll", (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodsWarpper.getElementsByClassName("food-list-hook");
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      }
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || this.scrollY >= height1 && this.scrollY < height2) {
+            return i;
+          }
+        }
+        console.log(1);
+        return 0;
+      }
     }
   };
 
@@ -82,6 +123,16 @@
         width: 56px;
         padding: 0 12px;
         line-height: 14px;
+        &.current {
+          position: relative;
+          margin-top: -1px;
+          z-index: 10;
+          background: #fff;
+          font-weight: 700;
+          .text {
+            @include boder-none();
+          }
+        }
         .icon {
           vertical-align: top;
           display: inline-block;
@@ -117,64 +168,65 @@
     }
     .foods-warpper {
       flex: 1;
-      .title{
-        padding-left:14px;
+      .title {
+        padding-left: 14px;
         height: 26px;
         line-height: 26px;
-        border-left:1px solid #d9dde1;
-        font-size:12px;
-        color: rgb(147,153,159);
+        border-left: 1px solid #d9dde1;
+        font-size: 12px;
+        color: rgb(147, 153, 159);
         background: #f3f5f7;
       }
-      .food-item{
+      .food-item {
         display: flex;
         margin: 18px;
         padding-bottom: 18px;
         @include border-1px(rgba(7, 17, 27, 0.1));
-        &:last-child{
+        &:last-child {
           @include boder-none();
           margin-bottom: 0;
         }
-        .icon{
+        .icon {
           flex: 0 0 57px;
           margin-right: 10px;
         }
-        .content{
+        .content {
           flex: 1;
-          .name{
+          .name {
             margin: 2px 0 8px 0;
             height: 14px;
             line-height: 14px;
             font-size: 14px;
-            color: rgb(7,17,27);
+            color: rgb(7, 17, 27);
           }
-          .desc,.extra{
+          .desc, .extra {
             height: 14px;
             line-height: 14px;
             font-size: 10px;
-            color:rgb(147,153,159);
+            color: rgb(147, 153, 159);
           }
-          .desc{
-            margin-bottom:8px;
+          .desc {
+            line-height: 12px;
+            margin-bottom: 8px;
           }
-          .extra{
+          .extra {
             line-height: 10px;
-            &.count{
+            .count {
               margin-left: 12px;
             }
           }
-          .price{
+          .price {
             font-weight: 700;
             line-height: 24px;
-            .now{
+            .now {
               margin-right: 8px;
               font-size: 14px;
-              color: rgb(240,20,20);
+              color: rgb(240, 20, 20);
             }
-            .old{
+            .old {
               text-decoration: line-through;
               font-size: 10px;
-              color:rgb(147,153,159);
+              color: rgb(147, 153, 159);
             }
           }
 
